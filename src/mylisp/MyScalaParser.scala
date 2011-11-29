d701ecbad3abee9cb941d3f2a1ef15620402fbba
takezoe <takezoe@gmail.com>
2011-11-30 01:35:59 +0900

    Add boolean literal and if expression.

--- src/mylisp/MyScalaParser.scala
+++ src/mylisp/MyScalaParser.scala
@@ -6,7 +6,8 @@ object MyLispParser extends App {
 
   val source = """
     (defun sayHello (name) (println "Hello " name "!"))
-    (sayHello "World")
+    (defun sum (a b)(if (eq a b) a (sum (+ a 1) b)))
+    (println (sum 1 1000))
   """
 
   val parser = new MyLispParser
@@ -17,35 +18,23 @@ object MyLispParser extends App {
   val env = new Environment()
 
   // define global functions
-  env.set("println", { params: List[Any] =>
-    println(params.mkString)
-  })
-  env.set("+", { params: List[Any] =>
-    params match {
-      case(List(a: Int, b: Int)) => a + b
-      case _ => throw new Exception("parameter is invalid.")
-    }
-  })
-  env.set("-", { params: List[Any] =>
-    params match {
-      case(List(a: Int, b: Int)) => a - b
-      case _ => throw new Exception("parameter is invalid.")
-    }
-  })
-  env.set("*", { params: List[Any] =>
-    params match {
-      case(List(a: Int, b: Int)) => a * b
-      case _ => throw new Exception("parameter is invalid.")
-    }
-  })
-  env.set("/", { params: List[Any] =>
-    params match {
-      case(List(a: Int, b: Int)) => a / b
-      case _ => throw new Exception("parameter is invalid.")
-    }
-  })
+  env.set("println", { params: List[Any] => println(params.mkString) })
+  env.set("+", operator2(_ + _))
+  env.set("-", operator2(_ - _))
+  env.set("*", operator2(_ * _))
+  env.set("/", operator2(_ / _))
+  env.set("eq", operator2(_ == _))
 
   new ExprVisitor().visit(ast, env)
+
+  private def operator2(f: (Int, Int) => Any): List[Any] => Any = {
+      (params: List[Any]) => {
+        params match {
+          case List(a: Int, b: Int) => f(a, b)
+          case _ => throw new Exception("Invalid arguments.")
+        }
+      }
+  }
 }
 
 class Environment(parent:Option[Environment] = None){
@@ -71,8 +60,8 @@ class Environment(parent:Option[Environment] = None){
 class ExprVisitor() {
   def visit(ast:AST, env: Environment):Any = {
     ast match {
-      case Expr(name, params) => {
-        env.get(name.name) match {
+      case Expr(ident, params) => {
+        env.get(ident.name) match {
           case f: Func => {
             val local = new Environment(Some(env))
             f.params.zip(params.map(visit(_, env))).foreach { case(variable, value) =>
@@ -81,11 +70,19 @@ class ExprVisitor() {
             visit(f.proc, local)
           }
           case f: ((List[Any]) => Any) => f(params.map(visit(_, env)))
-          case _ => throw new Exception("function '%s' not found.".format(name.name))
+          case _ => throw new Exception("function '%s' not found.".format(ident.name))
+        }
+      }
+      case If(cond, expr1, expr2) => {
+        if(visit(cond, env).asInstanceOf[Boolean] == true){
+          visit(expr1, env)
+        } else {
+          visit(expr2, env)
         }
       }
       case IntVal(value) => value
       case StrVal(value) => value
+      case BooleanVal(value) => value
       case Ident(name) => env.get(name)
       case Defun(name, func) => env.set(name.name, func)
       case Program(exprs) => exprs.foreach(visit(_, env))
@@ -96,23 +93,27 @@ class ExprVisitor() {
 trait AST
 case class IntVal(value: Int) extends AST
 case class StrVal(value: String) extends AST
+case class BooleanVal(value: Boolean) extends AST
 case class Ident(name: String) extends AST
 case class Expr(name: Ident, params:List[AST]) extends AST
 case class Defun(name: Ident, func: Func) extends AST
 case class Func(params:List[Ident], proc:AST) extends AST
+case class If(cond:AST, expr1:AST, expr2:AST) extends AST
 case class Program(exprs: List[AST]) extends AST
 
 class MyLispParser extends RegexParsers {
 
   def ident :Parser[Ident] = """[A-Za-z_+\-*/][a-zA-Z0-9]*""".r^?{
-    case n if n != "defun" => n
+    case n if n != "defun" && n != "if" => n
   }^^Ident
 
   def intLiteral : Parser[AST] = """[1-9][0-9]*|0""".r^^{ value => IntVal(value.toInt) }
 
+  def booleanLiteral : Parser[AST] = ("true"|"false")^^{ value => BooleanVal(value.toBoolean) }
+
   def stringLiteral : Parser[AST] = "\""~>"""[a-zA-Z0-9:*/+\- !]*""".r<~"\""^^StrVal
 
-  def value: Parser[AST] = expr|intLiteral|stringLiteral|ident
+  def value: Parser[AST] = expr|intLiteral|stringLiteral|booleanLiteral|ident
 
   def defun: Parser[AST] = ("(defun" ~> ident ~"("~ opt(rep(ident)) ~ ")" ~ expr <~")")^^{
     case(ident~_~params~_~proc) => {
@@ -120,7 +121,10 @@ class MyLispParser extends RegexParsers {
     }
   }
 
-  def expr: Parser[AST] = defun|("(" ~> ident ~ opt(rep(value)) <~ ")" )^^{
+  def `if`: Parser[AST] = ("(if" ~> value ~ value ~ value <~ ")")^^{
+    case(cond~expr1~expr2) => If(cond, expr1, expr2)
+  }
+  def expr: Parser[AST] = defun|`if`|("(" ~> ident ~ opt(rep(value)) <~ ")" )^^{
     case(ident~params) => {
       Expr(ident.asInstanceOf[Ident], params.get)
     }
